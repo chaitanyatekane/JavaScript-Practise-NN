feab850bf804719635629549c7322deb480f392c
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-09 07:48:33 +0530

    variables, constants & comments

---
+++ Lesson-07/script.js
@@ -0,0 +1,36 @@
+// Variables, Constants & Comments in JS
+
+// Variable
+let age = 25;
+console.log(age);
+
+let year = 2021;
+console.log(year);
+
+age = 30; 
+
+console.log(age, year);
+
+// Constant
+const points = 100;
+console.log(points);
+
+// points = 200; // TypeError // Cannot change const variable
+
+
+var score = 75; // older way before let and const
+console.log(score);
+
+// No space between variable name 
+// Use camelcasing e.g. newYear
+// Variables cannot start with number
+// Don't use reserved keywords
+// Try to name your variable meaningful
+
+// This is single line comment
+
+/*
+This is multiline comment
+Right, Yes!
+*/
+
