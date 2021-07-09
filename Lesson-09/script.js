ef384bec249542bb04df414c9e7f7c7f7b3036b3
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-09 08:11:34 +0530

    string basics

---
+++ Lesson-09/script.js
@@ -0,0 +1,26 @@
+// strings
+console.log('Hello, World');
+let email = 'chaitanyatekne5@gmail.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Chaitanya';
+let lastName = 'Tekane';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+console.log(fullName[1]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
