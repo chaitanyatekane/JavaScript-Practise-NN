258ebf6fc7c89bffac0eb7e85c42a686bb8f2a1b
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-09 14:17:52 +0530

    null and undefined

---
+++ Lesson-14/script.js
@@ -0,0 +1,9 @@
+// null and undefined
+
+let age;
+console.log(age, age+3, `the age is ${age}`);
+// undefined NaN "the age is undefined"
+
+let ageNew = null;
+console.log(ageNew, ageNew+3, `the age is ${ageNew}`);
+// null 3 "the age is null"
