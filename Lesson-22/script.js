3c68ff7b0e0f9b59dfeff8d1bef202af93831e59
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-10 07:51:05 +0530

    if statements

---
+++ Lesson-22/script.js
@@ -0,0 +1,19 @@
+// if statements
+
+const age = 25;
+if(age<30){
+    console.log("you are over 20");
+}
+// you are over 20
+
+const arr = ['chaitanya', 'mario', 'harry'];
+if(arr.length>2){
+    console.log("great");
+}
+// great
+
+const wordLen = 'password';
+if(wordLen.length>7){
+    console.log("that's awesome password");
+}
+// that's awesome password
