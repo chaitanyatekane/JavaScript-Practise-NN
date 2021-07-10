a90f3789389e6eeb995b9b6a3535ae7b037ca3fc
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-10 08:20:22 +0530

    logical NOT

---
+++ Lesson-25/script.js
@@ -0,0 +1,20 @@
+// logical NOT(!)
+
+let isTrue = false;
+if(isTrue){
+    console.log('True');
+}else{
+    console.log('False');
+}
+// False
+
+let isFalse = false;
+if(!isFalse){
+    console.log('True');
+}else{
+    console.log('False');
+}
+// True
+
+console.log(!true); // false
+console.log(!false); // true
