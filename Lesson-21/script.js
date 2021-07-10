086e7aab1188b9903aa99deb872de8a36b33afa4
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-10 07:43:25 +0530

    do-while loops

---
+++ Lesson-21/script.js
@@ -0,0 +1,20 @@
+// do-while loops
+
+let j = 0;
+do{
+    console.log(j);
+    j++;
+}while(j<6);
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+
+let i = 5;
+do{
+    console.log(i);
+    i++;
+}while(i<5);
+// 5
