6e623b337c406fd73d042350bbe2a185810f9a00
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-10 07:38:31 +0530

    while loops

---
+++ Lesson-20/script.js
@@ -0,0 +1,22 @@
+// while loops
+
+let i = 0;
+while(i<5){
+    console.log("value of i is", i);
+    i++;
+}
+// value of i is 0
+// value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+
+const names = ['mario', 'harry', 'chaitanya'];
+let j = 0;
+while(j<names.length){
+    console.log("names is", names[j]);
+    j++;
+}
+// names is mario
+// names is harry
+// names is chaitanya
