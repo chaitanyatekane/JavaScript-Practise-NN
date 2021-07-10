44655a2831080cbe7819825b09a72f10ed4f08a0
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-07-10 14:22:18 +0530

    switch statements

---
+++ Lesson-27/script.js
@@ -0,0 +1,105 @@
+// switch statements
+
+const currValue = 'D';
+switch(currValue){
+    case 'A':
+        console.log("That's A");
+        break;
+    case 'B': 
+        console.log("That's B");   
+        break;
+    case 'C':
+        console.log("That's C");
+        break;
+    case 'D':
+        console.log("That's D");
+        break;
+    case 'E':
+        console.log("That's E");
+        break;
+    case 'F':
+        console.log("That's F");
+        break;
+    default:
+        console.log("No Input Character");
+        break;
+}
+// That's D
+
+const newValue = 'A';
+switch(newValue){
+    case 'A':
+        console.log("That's A");
+        break;
+    case 'B': 
+        console.log("That's B");   
+        break;
+    case 'C':
+        console.log("That's C");
+        break;
+    case 'D':
+        console.log("That's D");
+        break;
+    case 'E':
+        console.log("That's E");
+        break;
+    case 'F':
+        console.log("That's F");
+        break;
+    default:
+        console.log("No Input Character");
+        break;
+}
+// That's A
+
+const newNumber = 4;
+switch(newNumber){
+    case 1:
+        console.log("That's 1");
+        break;
+    case 2: 
+        console.log("That's 2");   
+        break;
+    case 3:
+        console.log("That's 3");
+        break;
+    case 4:
+        console.log("That's 4");
+        break;
+    case 5:
+        console.log("That's 5");
+        break;
+    case 6:
+        console.log("That's 6");
+        break;
+    default:
+        console.log("No Input Number");
+        break;
+}
+// That's 4
+
+const newNumberValue = null;
+switch(newNumberValue){
+    case 1:
+        console.log("That's 1");
+        break;
+    case 2: 
+        console.log("That's 2");   
+        break;
+    case 3:
+        console.log("That's 3");
+        break;
+    case 4:
+        console.log("That's 4");
+        break;
+    case 5:
+        console.log("That's 5");
+        break;
+    case 6:
+        console.log("That's 6");
+        break;
+    default:
+        console.log("No Input Number");
+        break;
+}
+// No Input Number
